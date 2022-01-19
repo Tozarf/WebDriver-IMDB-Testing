0dbf75735b12e9486820d0aec4fdbbf7355b2781
Tozarf <fazatojuni@gmail.com>
2022-01-19 12:22:35 -0600

    Still working on browser execution 'jwplayer is not defined'

--- test/pageobjects/darkKnight.page.js
+++ test/pageobjects/darkKnight.page.js
@@ -1,20 +1,16 @@
 class DarkKnightPage {
     get playTrailerButton() {
-        return $(
-            "//*[@id='__next']/main/div/section[1]/section/div[3]/section/section/div[3]/div[1]/div/div[2]/div[2]/a/div[3]"
-        );
+        return $("//span[contains(text(),'Play trailer')]");
     }
-    get playButton() {
-        return $(
-            "/html[1]/body[1]/div[1]/div[3]/div[1]/main[1]/div[1]/section[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[9]/div[4]/div[2]/div[1]"
-        );
+    get videoContainer() {
+        return $("//video[@class='jw-video jw-reset']");
     }
 
     async clickPlayTrailerButton() {
         await this.playTrailerButton.waitForExist();
         await this.playTrailerButton.click();
     }
-    async clickPlayButton() {
+    async clickVideoContainer() {
         await this.playButton.waitForExist();
         await this.playButton.click();
     }
--- test/specs/DarkKnightVideoPlayer.js
+++ test/specs/DarkKnightVideoPlayer.js
@@ -10,11 +10,17 @@ describe("Verify the correct display of The Dark Knight", () => {
         await top250Page.clickDarkKnight();
 
         await DarkKnightPage.clickPlayTrailerButton();
+        const playerState = await browser.execute(() => {
+            return jwplayer().getState();
+        });
+        console.log(playerState);
+
+        // expect(playerState).toEqual("idle");
     });
     it("Validate that the jwplayer is displayed and the state is 'playing'", async () => {
-        await homePage.open();
-        await homePage.submitSearch("The Dark Knight");
-        await DarkKnightPage.clickPlayTrailerButton();
-        await DarkKnightPage.clickPlayButton();
+        // await homePage.open();
+        // await homePage.submitSearch("The Dark Knight");
+        // await DarkKnightPage.clickPlayTrailerButton();
+        // await DarkKnightPage.clickPlayButton();
     });
 });
